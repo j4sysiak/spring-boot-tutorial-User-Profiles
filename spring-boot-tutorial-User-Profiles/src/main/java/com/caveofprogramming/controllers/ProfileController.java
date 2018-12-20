82a1023fdf5247ca34d43b980cb5a06e91c743e1
j4sysiak <j4sysiak@gmail.com>
2018-12-20 13:02:37 +0100

    fsdfsd

---
+++ spring-boot-tutorial-User-Profiles/src/main/java/com/caveofprogramming/controllers/ProfileController.java
@@ -0,0 +1,80 @@
+package com.caveofprogramming.controllers;
+
+import org.springframework.data.auditing.config.AuditingHandlerBeanDefinitionParser;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ProfileController {
+	 
+	@RequestMapping(value="/profile")
+	public ModelAndView showProfile(ModelAndView modelAndView) {
+		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+		String username = auth.getName();
+		
+		modelAndView.getModel().put("message", username);
+		
+//		SiteUser user = getUser();
+//		Profile profile = profileService.getUserProfile(user);
+//		
+//		if(profile == null) {
+//			profile = new Profile();
+//			profile.setUser(user);
+//			profileService.save(profile);
+//		}
+//		
+//		Profile webProfile = new Profile();
+//		webProfile.safeCopyFrom(profile);
+//		
+//		modelAndView.getModel().put("profile", webProfile);
+//		
+//		modelAndView.setViewName("app.profile");
+		
+		modelAndView.setViewName("app.message");
+		return modelAndView;
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
